d004a4c0966bf0d6132e018549bd81b7571b55c4
Maribel Cruz <cruzg8562@gmail.com>
2026-03-17 22:47:30 -0600

    cambio

--- laboratorio1C2/vite.config.js
+++ laboratorio1C2/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'Laboratorio1C2',
+  base:'/Laboratorio1C2/',
   plugins: [
     vue(),
     vueDevTools(),
